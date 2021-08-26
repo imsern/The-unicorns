bb7dc2ee6dcd75b4cb1a1e740b26a5d7723b7056
cburull <88709887+cburull@users.noreply.github.com>
2021-08-26 22:09:09 +0200

    puzzle#2

--- Unicorn puzzle/script.js
+++ Unicorn puzzle/script.js
@@ -4,56 +4,60 @@ const brett=
 const brikker=
 ['p1','p2','p3','p4','q1','q2','q3','q4','r1','r2','r3','r4','s1','s2','s3','s4'];
 
-var bildeNr=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16];
+const riktigeBildeNr=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16];
+const tilfeldigeBildeNr=[...riktigeBildeNr];
 
-var valgtbrikke='';
-var forrigebrikke;
+var brikkeHTML='';
+var brikkeID='';
 
 function fyllBrikker(){
     for (i in brikker){
-        document.getElementById(brikker[i]).innerHTML=bildeNr[i];
+        document.getElementById(brikker[i]).innerHTML=tilfeldigeBildeNr[i];
+        // var navnpåbilde = 'unicorn'+tilfeldigeBildeNr[i]+'.png';
+        // document.getElementById(brikker[i]).innerHTML=`
+        // <img src="/images/Pieces/${navnpåbilde}" alt="brikke">
+        // `;
     }
 }
+function klikk(denne){
+    if (denne.innerHTML!='') velgbrikke(denne);
+    else limInn(denne);
+}
+
 function velgbrikke(brikke){
-    console.log(brikke.innerHTML);
-    for (i in brikker){
-        document.getElementById(brikker[i]).classList.remove('valgtbrikke');
-    }
-    if (brikke.innerHTML!=''){
-        valgtbrikke=brikke.innerHTML;
-        forrigebrikke=brikke.id;
-        brikke.classList.add('valgtbrikke');
-    }
+    if (brikkeID!=''){
+        document.getElementById(brikkeID).classList.remove('valgtBrikke');}
+    brikke.classList.add('valgtBrikke');
+    brikkeHTML=brikke.innerHTML;
+    brikkeID=brikke.id;
 }
+
 function limInn(rute){
-    if (rute.classList.contains('harbilde')==false && valgtbrikke !=''){
-        rute.innerHTML=valgtbrikke;
-        valgtbrikke='';
-        document.getElementById(forrigebrikke).classList.remove('valgtbrikke');
-        document.getElementById(forrigebrikke).innerHTML='';
-        rute.classList.add('harbilde');
-        rute.classList.add(forrigebrikke);
-        var victorycheck=0;
-        for (i in brett){
-            var x=document.getElementById(brett[i])
-            if (x.classList.contains(brikker[i])) victorycheck++;
-        if (victorycheck==brett.length) alert('HAHAHAHAHA');  //monkaS
-        }
+    if (brikkeHTML !=''){
+        rute.innerHTML=brikkeHTML;
+        brikkeHTML='';
+        document.getElementById(brikkeID).innerHTML='';
+        document.getElementById(brikkeID).classList.remove('valgtBrikke');
+        testVinn();
     }
 }
-function shuffle(array) {
-    var currentIndex = bildeNr.length, randomIndex;
 
-    //Så lenge det er elementer igjen å shuffle...
-    while (currentIndex != 0) {
-        //velg et random element av de som er igjen
-        randomIndex = Math.floor(Math.random()*currentIndex);
-        currentIndex--;
-        //bytt det ut med current element
-        [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];  //wtf er det her?
+function tilfeldiggjørBildeNr() {
+    var kopiBildeNr=[...tilfeldigeBildeNr];
+    for (i in tilfeldigeBildeNr){
+        var randomIndex=Math.floor(Math.random()*kopiBildeNr.length);
+        tilfeldigeBildeNr[i]=kopiBildeNr.splice(randomIndex,1)[0];
     }
-    return array;
-    console.log(bildeNr)
 }
 
-
+function testVinn(){
+    var antallRiktige=0;
+    for (i in brett){
+        var bildeHTML = riktigeBildeNr[i];
+        // var bildeNavn = 'unicorn'+riktigeBildeNr[i]+'.png';
+        // var bildeHTML=`<img src="/images/Pieces/${bildeNavn}" alt="brikke">`;
+        var ruteHTML = document.getElementById(brett[i]).innerHTML;
+        if (ruteHTML==bildeHTML) antallRiktige++;
+    }
+    if (antallRiktige==brett.length) alert('Installing Virus!')
+}
