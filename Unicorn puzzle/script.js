981869b6c3476ca9ce87518f3fe1df83ad2942e1
cburull <88709887+cburull@users.noreply.github.com>
2021-08-27 09:52:34 +0200

    riktig sti til bildene

--- Unicorn puzzle/script.js
+++ Unicorn puzzle/script.js
@@ -15,7 +15,7 @@ function fyllBrikker(){
         // document.getElementById(brikker[i]).innerHTML=tilfeldigeBildeNr[i];
         var navnpåbilde = tilfeldigeBildeNr[i]+'.png';
         document.getElementById(brikker[i]).innerHTML=`
-        <img src="/Images/Pieces/${navnpåbilde}" alt="brikke">
+        <img src="Images/Pieces/${navnpåbilde}" alt="brikke">
         `;
     }
 }
@@ -55,7 +55,7 @@ function testVinn(){
     for (i in brett){
         // var bildeHTML = riktigeBildeNr[i];
         var bildeNavn = riktigeBildeNr[i]+'.png';
-        var bildeHTML=`<img src="/Images/Pieces/${bildeNavn}" alt="brikke">`;
+        var bildeHTML=`<img src="Images/Pieces/${bildeNavn}" alt="brikke">`;
         var ruteHTML = document.getElementById(brett[i]).innerHTML;
         if (ruteHTML==bildeHTML) antallRiktige++;
     }
