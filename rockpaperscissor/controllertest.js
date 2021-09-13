a964121c0000a09763910ff84d3af332e79f4b18
imsern <kristofferims@hotmail.com>
2021-09-13 14:55:42 +0200

    Unit test update

--- rockpaperscissor/controllertest.js
+++ rockpaperscissor/controllertest.js
@@ -1,5 +1,4 @@
 function compareChoices(playerChoice){
-    let aiChoice = Math.floor(Math.random()*wordIndex.length);
     resultat = `You played ${wordIndex[playerChoice]} - AI played ${wordIndex[aiChoice]}. `;
     if (playerChoice > 0 && playerChoice == (aiChoice+1) ||  playerChoice == 0 && aiChoice == 2){
         resultat += `You won!`;
@@ -17,12 +16,17 @@ function compareChoices(playerChoice){
 
 function checkWin(){
     if (score[0] >= 3){
-        return 'You won';
-        
+        return 'You won';       
     } 
     else if (score[1] >= 3){
-        return 'AI won'
+        return 'AI won';
         
     }
     else return;
 }
+
+
+// sjekke at score går opp for player om player vinner og motsatt
+// sjekke alle forskjellige kombinasjoner "rock vs scissors" osv funker som de skal på begge
+// sette aiChoice til de forskjellige ordene, og endre verdien i unit test
+// currentround
